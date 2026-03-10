a8da0f867e47b4163dc86e0245a1fb96de023d0b
nirey-l <lyr725@gmail.com>
2026-03-10 17:46:22 +0900

    springboot add

--- src/main/java/com/basic/myspringboot/entity/Customer.java
+++ src/main/java/com/basic/myspringboot/entity/Customer.java
@@ -3,10 +3,12 @@ package com.basic.myspringboot.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
 @Table(name = "customers")
 @Getter @Setter
+@DynamicUpdate
 public class Customer {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,6 +17,6 @@ public class Customer {
     private String customerId;
 
     @Column(nullable = false)
-    private String cutomerName;
+    private String customerName;
 
 }
