e3e780bbfc9cd21dfb7af4d28e964f2fcc06279d
nirey-l <lyr725@gmail.com>
2026-03-11 21:44:35 +0900

    springboot add

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,14 +1,18 @@
 package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.repository.CustomerRepository;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -34,4 +38,37 @@ public class UserRestController {
     public List<User> getUsers() {
         return userRepository.findAll();
     }
+
+    //Id로 User 조회
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        User existUser = getExistUser(optionalUser);
+        return existUser;
+    }
+
+    //Email 조회하고 User 수정하기
+    @PatchMapping("/{email}/")
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail){
+        User existUser = getExistUser(userRepository.findByEmail(email));
+        existUser.setName(userDetail.getName());
+        return userRepository.save(existUser);
+    }
+
+    //User 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        User user = getExistUser(optionalUser);
+        userRepository.delete(user);
+        return ResponseEntity.ok("Id = " + id + " User가 삭제 되었습니다.");
+    }
+
+    //private 공통 메서드
+    private static User getExistUser(Optional<User> optionalUser) {
+        User existUser = optionalUser //Optional<User>
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));//User
+        return existUser;
+    }
+
 }
